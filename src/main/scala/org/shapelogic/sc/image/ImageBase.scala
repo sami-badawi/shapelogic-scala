f7eff9052efac96ab789b55fba2389ddb7e8380e
SamiBadawi <sami.badawi@gmail.com>
2016-06-26 06:58:43 -0400

    Formatting

--- src/main/scala/org/shapelogic/sc/image/ImageBase.scala
+++ src/main/scala/org/shapelogic/sc/image/ImageBase.scala
@@ -4,11 +4,11 @@ trait ImageBase[@specialized T] {
   def frozen: Boolean
 
   def channels: Int
-  
+
   def getChannel(x: Int, y: Int, ch: Int): T
 
   def setChannel(x: Int, y: Int, ch: Int, value: T): Unit
-  
+
   /**
    * Fill whole image with color
    */
