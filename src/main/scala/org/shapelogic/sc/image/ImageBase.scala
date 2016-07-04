34129473d410b2fbed4f80e0e6997ec02b1e02c8
Sami Badawi <sami.badawi@gmail.com>
2016-07-04 06:44:58 -0400

    Add Scala doc

--- src/main/scala/org/shapelogic/sc/image/ImageBase.scala
+++ src/main/scala/org/shapelogic/sc/image/ImageBase.scala
@@ -9,6 +9,10 @@ trait ImageBase[@specialized T] {
 
   def channels: Int
 
+  /**
+   * Get the first channel if this is byte array
+   * If it is an Int array with bytes packed in it would be the Int
+   */
   def getIndex(x: Int, y: Int): Int
 
   def getChannel(x: Int, y: Int, ch: Int): T
