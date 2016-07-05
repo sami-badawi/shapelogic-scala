467e128c7e9a070f4fa947c58b6e354cde4102d3
Sami Badawi <sami.badawi@gmail.com>
2016-07-04 23:20:51 -0400

    Made a first wrapper image

---
+++ src/main/scala/org/shapelogic/sc/image/GrayByteBufferedImage.scala
@@ -0,0 +1,31 @@
+package org.shapelogic.sc.image
+
+import java.awt._
+import java.awt.event._
+import java.awt.image._
+import java.io._
+import javax.imageio._
+
+/**
+ * Wrapper
+ */
+class GrayByteBufferedImage(bufferedImage: BufferedImage) extends ReadImage[Byte] {
+  lazy val width: Int = bufferedImage.getWidth
+  lazy val height: Int = bufferedImage.getHeight
+  
+  def frozen: Boolean = false
+
+  def numBands: Int = 1
+
+  def getIndex(x: Int, y: Int): Int = {
+    width * y + x
+  }
+
+  def getChannel(x: Int, y: Int, ch: Int): Byte = {
+    bufferedImage.getData.getSample(x, y, 0).toByte
+  }
+
+  def getPixel(x: Int, y: Int): Array[Byte] = {
+    Array[Byte](getChannel(x, y, ch = 0))
+  }
+}
