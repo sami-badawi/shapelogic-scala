72d293565c0da3d0eb61682a6cb137b7822051c7
Sami Badawi <sami.badawi@gmail.com>
2016-07-05 20:27:56 -0400

    Real image file loader to wrapped image

--- src/main/scala/org/shapelogic/sc/io/LoadImage.scala
+++ src/main/scala/org/shapelogic/sc/io/LoadImage.scala
@@ -7,6 +7,7 @@ import java.io._
 import javax.imageio._
 import scala.util.Try
 import org.shapelogic.sc.image.RGBNumImage
+import org.shapelogic.sc.image.RGBIntBufferedImage
 
 object LoadImage {
 
@@ -39,9 +40,40 @@ object LoadImage {
       None
   }
 
+  def bufferedImageToRGBIntImage(bufferedImage: BufferedImage): Option[RGBIntBufferedImage] = {
+    val colorModel = bufferedImage.getColorModel
+    println(s"colorModel: $colorModel")
+    if (bufferedImage.getColorModel == BufferedImage.TYPE_INT_RGB)
+      try {
+        val res = new RGBIntBufferedImage(bufferedImage)
+        Some(res)
+      } catch {
+        case ex: Throwable => {
+          println(ex.getMessage)
+          ex.printStackTrace()
+          None
+        }
+      }
+    else
+      None
+  }
+
   def main(args: Array[String]): Unit = {
     println(s"args: ${args.toSeq}")
     val filename = args(0)
-    loadFile(filename)
+    val imageOpt = loadFile(filename).toOption
+    imageOpt match {
+      case Some(image) => {
+        val wrappedOpt = bufferedImageToRGBIntImage(image)
+        wrappedOpt match {
+          case Some(wrapped) => {
+            val pointRGB = wrapped.getPixel(10, 10).toSeq
+            println(s"Image loaded. RGB: $pointRGB")
+          }
+          case None => println("Very strange")
+        }
+      }
+      case None => println("Image could not be loaded")
+    }
   }
 }
