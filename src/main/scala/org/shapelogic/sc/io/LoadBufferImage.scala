27fd92e5f8c0b4f6b7799136df4db498f6f49ded
Sami Badawi <sami.badawi@gmail.com>
2016-07-09 22:51:30 -0400

    Wire up loader for new BufferImage

--- src/main/scala/org/shapelogic/sc/io/LoadBufferImage.scala
+++ src/main/scala/org/shapelogic/sc/io/LoadBufferImage.scala
@@ -11,6 +11,8 @@ import org.shapelogic.sc.image.RGBIntBufferedImage
 import java.awt.image.Raster
 import java.awt.image.DataBufferByte
 import org.shapelogic.sc.image.ReadImage
+import org.shapelogic.sc.image.BufferImage
+import org.shapelogic.sc.image._
 
 /**
  * BufferImage is the workhorse image type
@@ -18,41 +20,38 @@ import org.shapelogic.sc.image.ReadImage
  */
 object LoadBufferImage {
 
-  def bufferedImageToRGBNumImage(bufferedImage: BufferedImage): Option[RGBNumImage[Byte]] = {
+  val coveredBufferedImageTypeSet: Set[Int] = Set(
+    BufferedImage.TYPE_3BYTE_BGR,
+    BufferedImage.TYPE_BYTE_GRAY)
+
+  def bufferedImageToBufferImage(bufferedImage: BufferedImage): Option[BufferImage[Byte]] = {
     val rgbType = bufferedImage.getType
     val colorModel = bufferedImage.getColorModel
     println(s"colorModel: $colorModel")
-    if (rgbType == BufferedImage.TYPE_3BYTE_BGR)
+    if (coveredBufferedImageTypeSet.contains(rgbType))
       Try({
         val raster = bufferedImage.getData
         val byteBuffer: Array[Byte] = LoadImage.rasterToByteArray(raster)
-        val res: RGBNumImage[Byte] = new RGBNumImage(width = bufferedImage.getWidth, height = bufferedImage.getHeight, bufferIn = byteBuffer)
+        val res: BufferImage[Byte] =
+          if (rgbType == BufferedImage.TYPE_3BYTE_BGR)
+            new BufferImage(
+              width = bufferedImage.getWidth,
+              height = bufferedImage.getHeight,
+              numBands = 3,
+              bufferInput = byteBuffer,
+              rgbOffsetsOpt = Some(rgbRGBOffsets))
+          else if (rgbType == BufferedImage.TYPE_BYTE_GRAY)
+            new BufferImage(
+              width = bufferedImage.getWidth,
+              height = bufferedImage.getHeight,
+              numBands = 3,
+              bufferInput = byteBuffer,
+              rgbOffsetsOpt = Some(grayRGBOffsets))
+          else null
         res
       }).toOption
     else
       None
-  }
-
-  def bufferedImageToRGBIntImage(bufferedImage: BufferedImage): Option[ReadImage[Byte]] = {
-    val colorModel = bufferedImage.getColorModel
-    val rgbType = bufferedImage.getType
-    println(s"colorModel: $colorModel, \nrgbType: $rgbType")
-    println(s"Expected: ${BufferedImage.TYPE_INT_RGB}")
-    try {
-      if (rgbType == BufferedImage.TYPE_INT_RGB) {
-        val res = new RGBIntBufferedImage(bufferedImage)
-        Some(res)
-      } else if (rgbType == BufferedImage.TYPE_3BYTE_BGR) {
-        bufferedImageToRGBNumImage(bufferedImage)
-      } else
-        None
-    } catch {
-      case ex: Throwable => {
-        println(ex.getMessage)
-        ex.printStackTrace()
-        None
-      }
-    }
   }
 
   def main(args: Array[String]): Unit = {
@@ -61,7 +60,7 @@ object LoadBufferImage {
     val imageOpt = LoadImage.loadFile(filename).toOption
     imageOpt match {
       case Some(image) => {
-        val wrappedOpt = bufferedImageToRGBIntImage(image)
+        val wrappedOpt = LoadImage.bufferedImageToRGBIntImage(image)
         wrappedOpt match {
           case Some(wrapped) => {
             val pointRGB = wrapped.getPixel(10, 10).toSeq
