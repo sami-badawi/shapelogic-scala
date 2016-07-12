3d24a440ce70b92e7a9f1c3ffa8b2d5e01eb2056
Sami Badawi <sami.badawi@gmail.com>
2016-07-11 20:51:49 -0400

    First version of a unit test. One is failing

---
+++ src/test/scala/org/shapelogic/sc/image/BufferImageSpec.scala
@@ -0,0 +1,23 @@
+package org.shapelogic.sc.image
+
+import org.scalatest._
+
+import spire.algebra._
+import spire.std._
+//import spire.math.Integral
+import spire.syntax.ring._
+//import spire.math.Numberic
+import spire.implicits._
+
+class BufferImageSpecSpec extends FunSuite with BeforeAndAfterEach {
+  test("Image get instantiated to 0") {
+    val grayByteImage = new BufferImage[Byte](15, 10, 1, null)
+    assertResult(0) { grayByteImage.getChannel(x = 0, y = 0, ch = 0) }
+  }
+
+  test("Image fill with 100") {
+    val grayByteImage = new BufferImage[Byte](15, 10, 1, null)
+    grayByteImage.fill(100)
+    assertResult(100) { grayByteImage.getChannel(x = 0, y = 0, ch = 0) }
+  }
+}
