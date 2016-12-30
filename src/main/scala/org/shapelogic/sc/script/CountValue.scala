7f2672371cdcf550bee31619e0860f2099670648
Sami Badawi <sami@citymaps.com>
2016-12-30 09:12:23 -0500

    Wired argument parsing up to CountValue example class

--- src/main/scala/org/shapelogic/sc/script/CountValue.scala
+++ src/main/scala/org/shapelogic/sc/script/CountValue.scala
@@ -1,11 +1,33 @@
 package org.shapelogic.sc.script
 
+import org.shapelogic.sc.util.Args
+
 /**
- * Example displaying creation of 
+ * Example displaying creation of
  */
 object CountValue {
-  
+
+  def countZero(filename: String): Long = {
+    0
+  }
+
+  def errorMessage(): Unit = {
+    Args.parser
+  }
+
   def main(args: Array[String]): Unit = {
-    
+    val paramOpt = Args.parser.parse(args, Args())
+    paramOpt match {
+      case Some(param) => {
+        val filename = param.input
+        if (!filename.isEmpty) {
+          val zeros = countZero(filename)
+          println(s"zero count: $zeros in $filename")
+        } else {
+          errorMessage()
+        }
+      }
+      case None => errorMessage()
+    }
   }
 }
