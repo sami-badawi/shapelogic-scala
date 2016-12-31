8b06062adb7b8b9ed6a2bf46e408eff8ed12afe1
Sami Badawi <sami@citymaps.com>
2016-12-30 21:50:29 -0500

    Add a few more cases

--- src/main/scala/org/shapelogic/sc/io/BufferedImageTypeTranslator.scala
+++ src/main/scala/org/shapelogic/sc/io/BufferedImageTypeTranslator.scala
@@ -1,7 +1,7 @@
 package org.shapelogic.sc.io
 
 import java.awt.image.BufferedImage
-import org.shapelogic.sc.image.{ RGBOffsets, rgbRGBOffsets, bgrRGBOffsets }
+import org.shapelogic.sc.image._
 
 object BufferedImageTypeTranslator {
 
@@ -9,6 +9,8 @@ object BufferedImageTypeTranslator {
     bufferedImageType match {
       case BufferedImage.TYPE_INT_RGB => Some(rgbRGBOffsets)
       case BufferedImage.TYPE_INT_BGR => Some(bgrRGBOffsets)
+      case BufferedImage.TYPE_3BYTE_BGR => Some(bgrRGBOffsets)
+      case BufferedImage.TYPE_4BYTE_ABGR => Some(abgrRGBOffsets)
       case other => None
     }
   }
