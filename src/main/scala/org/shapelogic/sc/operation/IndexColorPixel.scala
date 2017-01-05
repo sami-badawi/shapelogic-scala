d5f3702644ff4a9aaff8b4328826d46f78694d14
Sami Badawi <sami@citymaps.com>
2017-01-04 19:34:35 -0500

    Added class IndexColorPixel

---
+++ src/main/scala/org/shapelogic/sc/operation/IndexColorPixel.scala
@@ -0,0 +1,37 @@
+package org.shapelogic.sc.operation
+
+import simulacrum._
+import scala.reflect.ClassTag
+import scala.reflect.runtime.universe._
+
+import org.shapelogic.sc.image.RGBOffsets
+import org.shapelogic.sc.image.BufferImage
+
+/**
+ * From buffer, index and RGBOffsets get all the color information as un-boxed numbers
+ */
+class IndexColorPixel[@specialized T: ClassTag](rgbOffsets: RGBOffsets, buffer: Array[T]) {
+  lazy val red = rgbOffsets.red
+  lazy val green = rgbOffsets.green
+  lazy val blue = rgbOffsets.blue
+  lazy val alpha = rgbOffsets.alpha
+
+  def getRed(index: Int): T = {
+    buffer.apply(index + red)
+  }
+
+  def getGreen(index: Int): T = {
+    buffer.apply(index + green)
+  }
+
+  def getBlue(index: Int): T = {
+    buffer.apply(index + blue)
+  }
+}
+
+object IndexColorPixel {
+
+  def apply[T: ClassTag](bufferImage: BufferImage[T]): IndexColorPixel[T] = {
+    new IndexColorPixel[T](bufferImage.getRGBOffsetsDefaults, bufferImage.data)
+  }
+}
