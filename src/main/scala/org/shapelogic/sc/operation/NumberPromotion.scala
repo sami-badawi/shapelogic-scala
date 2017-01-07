b931ce462359443670871a344979b50d0a626325
Sami Badawi <sami@citymaps.com>
2017-01-07 08:43:10 -0500

    More work on number promotion

--- src/main/scala/org/shapelogic/sc/operation/NumberPromotion.scala
+++ src/main/scala/org/shapelogic/sc/operation/NumberPromotion.scala
@@ -1,5 +1,9 @@
 package org.shapelogic.sc.operation
 
+import spire.math.Numeric
+import scala.specialized
+import scala.reflect.ClassTag
+
 trait NumberPromotion[I] {
   type Output
   def promote(input: I): Output
@@ -12,6 +16,13 @@ object NumberPromotion {
     type Output = Int
     def promote(input: Byte): Int = {
       byteMask & byteMask
+    }
+  }
+
+  class NumberPromotionIdentity[@specialized N: ClassTag: Numeric] extends NumberPromotion[N] {
+    type Output = N
+    def promote(input: N): N = {
+      input
     }
   }
 }
