e68a56a73f98dba5f0f1cfe378ffaf69ed52dae4
Sami Badawi <sami@citymaps.com>
2017-01-07 12:49:30 -0500

    Added Lemma pattern

--- src/main/scala/org/shapelogic/sc/operation/NumberPromotion.scala
+++ src/main/scala/org/shapelogic/sc/operation/NumberPromotion.scala
@@ -4,23 +4,40 @@ import spire.math.Numeric
 import scala.specialized
 import scala.reflect.ClassTag
 
+/**
+ * You frequently have to change from
+ */
 trait NumberPromotion[I] {
-  type Output
-  def promote(input: I): Output
+  /*
+   * Do not know this type in advance
+   */
+  type Out
+
+  /**
+   * Promote to a place that is better for computation
+   * One use is to fix signed Byte to unsigned Byte
+   */
+  def promote(input: I): Out
 }
 
 object NumberPromotion {
+
+  /**
+   * Lemma pattern
+   */
+  type Aux[I, O] = NumberPromotion[I] { type Out = O }
+
   val byteMask: Int = 0xff
 
   object BytePromotion extends NumberPromotion[Byte] {
-    type Output = Int
+    type Out = Int
     def promote(input: Byte): Int = {
       byteMask & byteMask
     }
   }
 
   class NumberPromotionIdentity[@specialized N: ClassTag: Numeric] extends NumberPromotion[N] {
-    type Output = N
+    type Out = N
     def promote(input: N): N = {
       input
     }
