caa68e44364299ca7aa5a06402534a5f9ca0dfe0
Sami Badawi <sami@citymaps.com>
2017-01-14 17:56:14 -0500

    Made implicit version of inverters for all primitive types

--- src/main/scala/org/shapelogic/sc/numeric/GenericInverse.scala
+++ src/main/scala/org/shapelogic/sc/numeric/GenericInverse.scala
@@ -18,6 +18,34 @@ object GenericInverse {
         (~input).toByte
       }
     }
+
+    implicit lazy val shortInverse: GenericFunction[Short] = new GenericFunction[Short] {
+      type Res = Short
+      def transform(input: Short): Short = {
+        (~input).toShort
+      }
+    }
+
+    implicit lazy val intInverse: GenericFunction[Int] = new GenericFunction[Int] {
+      type Res = Int
+      def transform(input: Int): Int = {
+        ~input
+      }
+    }
+
+    implicit lazy val floatInverse: GenericFunction[Float] = new GenericFunction[Float] {
+      type Res = Float
+      def transform(input: Float): Float = {
+        1.0f - input
+      }
+    }
+
+    implicit lazy val doubleInverse: GenericFunction[Double] = new GenericFunction[Double] {
+      type Res = Double
+      def transform(input: Double): Double = {
+        1.0 - input
+      }
+    }
   }
 
 }
