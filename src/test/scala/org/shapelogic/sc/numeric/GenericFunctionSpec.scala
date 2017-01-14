2f1761e0da46816d4d3339ae9c5f0a9318df6d64
Sami Badawi <sami@citymaps.com>
2017-01-14 17:32:33 -0500

    Create typeclass and use simulacrum and created type classes

--- src/test/scala/org/shapelogic/sc/numeric/GenericFunctionSpec.scala
+++ src/test/scala/org/shapelogic/sc/numeric/GenericFunctionSpec.scala
@@ -7,16 +7,25 @@ import spire.math.Numeric
 
 class GenericFunctionSpec extends FunSuite with BeforeAndAfterEach {
 
-  test("") {
-    implicit val genericFunctionEx = new GenericFunction[Byte] {
-      type Res = Int
-      val byteMask: Int = 0xff
-      def transform(input: Byte): Int = {
-        input & byteMask
-      }
+  lazy val bytePromoterGenericFunction: GenericFunction[Byte] = new GenericFunction[Byte] {
+    type Res = Int
+    val byteMask: Int = 0xff
+    def transform(input: Byte): Int = {
+      input & byteMask
     }
+  }
+
+  test("Create typeclass and use directly") {
+    implicit val genericFunctionEx = bytePromoterGenericFunction
     val byte: Byte = -1
     assertResult(255) { genericFunctionEx.transform(byte) }
+  }
+
+  test("Create typeclass and use simulacrum and promoted type classes") {
+    implicit val genericFunctionEx = bytePromoterGenericFunction
+    import GenericFunction.ops._
+    val byte: Byte = -1
+    assertResult(255) { byte.transform }
   }
 
 }
