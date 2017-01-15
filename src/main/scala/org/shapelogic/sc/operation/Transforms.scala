f905ed70c9501f00a087878837c9caa5e79d819d
Sami Badawi <sami@citymaps.com>
2017-01-15 04:40:41 -0500

    Took out unused code and added Scala doc

--- src/main/scala/org/shapelogic/sc/operation/Transforms.scala
+++ src/main/scala/org/shapelogic/sc/operation/Transforms.scala
@@ -14,18 +14,22 @@ import org.shapelogic.sc.numeric.GenericFunction
 import org.shapelogic.sc.numeric.GenericFunction._
 
 object Transforms {
+  
+  /**
+   * This is redundant now, but the generic only worked after adding context bound on TransFunction
+   */
   def makeInverseTransformByte(
     inputImage: BufferImage[Byte]): SimpleTransform[Byte] = {
     type T = Byte
     import GenericInverse.DirectInverse._
-    import TransFunction.ops._
-
     val genericFunction: TransFunction[T] = implicitly[TransFunction[T]]
     val function: T => T = genericFunction.transform
-    val res = new SimpleTransform[T](inputImage)(function)
-    res
+    new SimpleTransform[T](inputImage)(function)
   }
 
+  /**
+   * First fully generic image operation
+   */
   def makeInverseTransform[@specialized(Byte, Short, Int, Long, Float, Double) T: ClassTag: Numeric: Ordering: TransFunction](
     inputImage: BufferImage[T]): SimpleTransform[T] = {
     import GenericInverse.DirectInverse._
