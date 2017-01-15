84d04a4a77f9fd5291402ce03f20038f4d339e94
Sami Badawi <sami@citymaps.com>
2017-01-15 10:21:53 -0500

    JavaFX image loader now takes command line parameters

--- src/main/scala/org/shapelogic/sc/javafx/SayHelloJavaFX.scala
+++ src/main/scala/org/shapelogic/sc/javafx/SayHelloJavaFX.scala
@@ -13,32 +13,60 @@ import javafx.scene.layout.Pane
 import javafx.scene.paint.Color
 import javafx.scene.image.Image
 
+import scala.collection.JavaConversions._
+import org.shapelogic.sc.util.Args
+
 /**
  * Class has to be separate from object for JavaFX to work
  */
 class SayHelloJavaFX extends Application {
 
+  /**
+   * Just the usual
+   */
+  def getMainArgs(): Array[String] = {
+    val parameters = getParameters()
+    val unNamed = parameters.getUnnamed
+    val seq = unNamed.toSeq
+    seq.toArray
+  }
+
+  def getParsedArgs(): Args = {
+    val args = getMainArgs()
+    val paramOpt = Args.parser.parse(args, Args())
+    paramOpt match {
+      case Some(param) => {
+        param
+      }
+      case None => {
+        null
+      }
+    }
+  }
+
   override def start(stage: Stage): Unit = {
-    val canvas = new Canvas(800, 600);
-    val filename = "image/output.png"
-    val image = new Image(s"file:$filename");
-    val gc: GraphicsContext = canvas.getGraphicsContext2D();
+    val parameters = getParameters()
+    val arguments = getParsedArgs()
+    val canvas = new Canvas(800, 600)
+    val filename = if (arguments.input == null || arguments.input.isEmpty) "image/output.png" else arguments.input
+    val image = new Image(s"file:$filename")
+    val gc: GraphicsContext = canvas.getGraphicsContext2D()
     gc.drawImage(image, 10, 10)
 
-    val root = new Pane();
+    val root = new Pane()
     // Set the Style-properties of the Pane
     root.setStyle("-fx-padding: 10;" +
       "-fx-border-style: solid inside;" +
       "-fx-border-width: 2;" +
       "-fx-border-insets: 5;" +
       "-fx-border-radius: 5;" +
-      "-fx-border-color: blue;");
+      "-fx-border-color: blue;")
 
-    root.getChildren().add(canvas);
-    val scene = new Scene(root);
-    stage.setScene(scene);
-    stage.setTitle("ShapeLogic Scala");
-    stage.show();
+    root.getChildren().add(canvas)
+    val scene = new Scene(root)
+    stage.setScene(scene)
+    stage.setTitle("ShapeLogic Scala")
+    stage.show()
   }
 }
 
