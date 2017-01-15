0d8b3238d87cc23112892934c53a11a3549d65bd
Sami Badawi <sami@citymaps.com>
2017-01-15 09:55:08 -0500

    Display image in JavaFX GUI

--- src/main/scala/org/shapelogic/sc/javafx/SayHelloJavaFX.scala
+++ src/main/scala/org/shapelogic/sc/javafx/SayHelloJavaFX.scala
@@ -7,30 +7,38 @@ import javafx.scene.Scene
 import javafx.scene.control.Button
 import javafx.scene.layout.StackPane
 import javafx.stage.Stage
+import javafx.scene.canvas.Canvas
+import javafx.scene.canvas.GraphicsContext
+import javafx.scene.layout.Pane
+import javafx.scene.paint.Color
+import javafx.scene.image.Image
 
 /**
  * Class has to be separate from object for JavaFX to work
  */
 class SayHelloJavaFX extends Application {
 
-  override def start(primaryStage: Stage): Unit = {
-    val btn = new Button()
-    btn.setText("Say 'Hello World'")
-    btn.setOnAction(new EventHandler[ActionEvent]() {
+  override def start(stage: Stage): Unit = {
+    val canvas = new Canvas(800, 600);
+    val filename = "image/output.png"
+    val image = new Image(s"file:$filename");
+    val gc: GraphicsContext = canvas.getGraphicsContext2D();
+    gc.drawImage(image, 10, 10)
 
-      override def handle(event: ActionEvent): Unit = {
-        println("Hello World!")
-      }
-    })
+    val root = new Pane();
+    // Set the Style-properties of the Pane
+    root.setStyle("-fx-padding: 10;" +
+      "-fx-border-style: solid inside;" +
+      "-fx-border-width: 2;" +
+      "-fx-border-insets: 5;" +
+      "-fx-border-radius: 5;" +
+      "-fx-border-color: blue;");
 
-    val root = new StackPane()
-    root.getChildren().add(btn)
-
-    val scene = new Scene(root, 300, 250)
-
-    primaryStage.setTitle("ShapeLogic Scala")
-    primaryStage.setScene(scene)
-    primaryStage.show()
+    root.getChildren().add(canvas);
+    val scene = new Scene(root);
+    stage.setScene(scene);
+    stage.setTitle("ShapeLogic Scala");
+    stage.show();
   }
 }
 
