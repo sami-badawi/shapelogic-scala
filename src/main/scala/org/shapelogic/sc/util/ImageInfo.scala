c8c0485306c45b462e762c22f33f52202d32cebb
Sami Badawi <sami@citymaps.com>
2017-01-16 10:47:22 -0500

    Added ImageInfo

---
+++ src/main/scala/org/shapelogic/sc/util/ImageInfo.scala
@@ -0,0 +1,41 @@
+package org.shapelogic.sc.util
+
+import java.awt.image._
+import org.shapelogic.sc.image.BufferImage
+import org.shapelogic.sc.image.ImageShape
+
+trait ImageInfo[T] {
+  def info(img: T, filename: String): String
+}
+
+/**
+ *
+ */
+object ImageInfo {
+
+  implicit lazy val bufferedImageImageInfo = new ImageInfo[BufferedImage] {
+    def info(img: BufferedImage, filename: String = ""): String = {
+      val colorModel = img.getColorModel
+      val width = img.getWidth
+      val height = img.getHeight
+      val res = s"BufferedImage info: width: $width height: $height colorModel: ${colorModel}"
+      if (filename != null && !filename.isEmpty())
+        s"$res, filename: $filename"
+      else
+        res
+    }
+  }
+
+  implicit lazy val bufferImageImageInfo = new ImageInfo[ImageShape] {
+    def info(img: ImageShape, filename: String = ""): String = {
+      val colorModel = img.rgbOffsetsOpt
+      val width = img.width
+      val height = img.height
+      val res = s"BufferedImage info: width: $width height: $height colorModel: ${colorModel}"
+      if (filename != null && !filename.isEmpty())
+        s"$res, filename: $filename"
+      else
+        res
+    }
+  }
+}
