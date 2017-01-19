69ce4c3cff814f7fca63ec9a2414a56522350ee5
Sami Badawi <sami@citymaps.com>
2017-01-18 22:33:57 -0500

    Package object for old with Scala doc

---
+++ src/main/scala/org/shapelogic/sc/old/old.scala
@@ -0,0 +1,10 @@
+package org.shapelogic.sc
+
+/**
+ * This is a packaged to re-factored classes in before deleting them
+ * 
+ * So they don't clutter up the rest of the project
+ */
+package object old {
+  
+}
