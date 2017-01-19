cef87da185b7b01cfee3412d5b0244e6b41d69e2
Sami Badawi <sami@citymaps.com>
2017-01-19 06:02:36 -0500

    Add Scala doc

--- src/main/scala/org/shapelogic/sc/pixel/PixelHandler1.scala
+++ src/main/scala/org/shapelogic/sc/pixel/PixelHandler1.scala
@@ -2,6 +2,13 @@ package org.shapelogic.sc.pixel
 
 import org.shapelogic.sc.image.RGBOffsets
 
+/**
+ * The idea is that you are transforming to the same output type
+ * But there is an inner type that you can do the calculation in
+ * 
+ * This is almost the same as NumberPromotionMax[I]
+ * But the names are different
+ */
 trait PixelHandler1[I] {
   type Out
 
@@ -23,5 +30,5 @@ trait PixelHandler1[I] {
 
   def i2Out(c: I): Out
   def out2i(c: Out): I
-  
+
 }
