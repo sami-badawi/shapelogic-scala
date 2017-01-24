7ebf120fbdda80cbf58f2bfd698bc702790ffa82
Sami Badawi <sami@citymaps.com>
2017-01-23 20:12:17 -0500

    Make PixelHandler useful

--- src/main/scala/org/shapelogic/sc/pixel/PixelHandler.scala
+++ src/main/scala/org/shapelogic/sc/pixel/PixelHandler.scala
@@ -1,14 +1,15 @@
 package org.shapelogic.sc.pixel
 
 import org.shapelogic.sc.image.RGBOffsets
+import org.shapelogic.sc.numeric.NumberPromotionMax
 
 /**
  * The idea is to have the logic in objects of this type
  * The the full operations can just be a runner
  */
 trait PixelHandler[I] {
-  type Calc
-  type Out
+  type C
+  def promoter: NumberPromotionMax.Aux[I, C]
 
   def data: Array[I]
 
@@ -28,19 +29,11 @@ trait PixelHandler[I] {
    * indexIn: index of input buffer
    * channelOut: channel number for output
    */
-  def calc(indexIn: Int, channelOut: Int): Out
-
-  def i2Calc(input: I): Calc
-  def calc2I(c: Calc): I
-
-  def calc2Out(c: Calc): Out
-  def out2Calc(c: Out): Calc
-
+  def calc(indexIn: Int, channelOut: Int): I
 }
 
 object PixelHandler {
-  type Aux[I, C, O] = PixelHandler[I] {
-    type Calc = C
-    type Res = O
+  type Aux[I, J] = PixelHandler[I] {
+    type C = J
   }
 }
