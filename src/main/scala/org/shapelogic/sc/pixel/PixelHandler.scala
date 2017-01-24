e309b0fefc7e20778411229cbf1c665905d5c4d0
Sami Badawi <sami@citymaps.com>
2017-01-23 23:52:53 -0500

    Problem with type unification. I to move inner abstract type out into type paramter

--- src/main/scala/org/shapelogic/sc/pixel/PixelHandler.scala
+++ src/main/scala/org/shapelogic/sc/pixel/PixelHandler.scala
@@ -8,9 +8,15 @@ import scala.reflect.ClassTag
 /**
  * The idea is to have the logic in objects of this type
  * The the full operations can just be a runner
+ *
+ * Started with a version that had: 
+ * abstract type C inside
+ * but that did not type check
+ * The types would not unify
  */
-trait PixelHandler[I] {
-  type C
+trait PixelHandler[I, C] {
+  //  type C //XXX did not type check
+
   def promoter: NumberPromotion.Aux[I, C]
 
   def data: Array[I]
@@ -36,17 +42,21 @@ trait PixelHandler[I] {
 }
 
 object PixelHandler {
-  type Aux[I, J] = PixelHandler[I] {
-    type C = J
+  
+  /**
+   * If I move the type C inside this would be useful again.
+   * Now it is just an abstraction
+   */
+  type Aux[I, C] = PixelHandler[I, C] {
+    //    type C = J //XXX did not type check
   }
 
   /**
    * Same input and Calc type
    * Will typically have a
    */
-  type Same[I] = PixelHandler[I] {
-    type C = I
-    def promoter: NumberPromotionMax.Aux[I, C]
+  type Same[I] = PixelHandler[I, I] {
+    //    type C = I
+    def promoter: NumberPromotionMax.Aux[I, I]
   }
-
 }
