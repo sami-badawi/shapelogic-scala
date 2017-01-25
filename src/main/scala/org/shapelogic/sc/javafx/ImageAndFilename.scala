ffc98cb5805fa14f226b6085397f10939ce52626
Sami Badawi <sami@citymaps.com>
2017-01-24 23:15:03 -0500

    Added lazy eval

--- src/main/scala/org/shapelogic/sc/javafx/ImageAndFilename.scala
+++ src/main/scala/org/shapelogic/sc/javafx/ImageAndFilename.scala
@@ -1,15 +1,43 @@
 package org.shapelogic.sc.javafx
 
+import org.shapelogic.sc.image.BufferImage
+import javafx.scene.image.Image
+
 /**
  * There are 2 image types
  * Since image seems to be saving in RGBA
  * And for some operations it is important that it is a specific type
  * The idea is that you will take the best image type for what you need.
- * 
- * So if you need to run an operation on an ImageAndFilename and it already 
+ *
+ * So if you need to run an operation on an ImageAndFilename and it already
  * is present in bufferImage format you do not need to translate it first.
  */
 case class ImageAndFilename(
-    bufferImage: org.shapelogic.sc.image.BufferImage[_],
-    image: javafx.scene.image.Image,
-    url: String)
+    bufferImage: BufferImage[_],
+    image: Image,
+    url: String) {
+
+  def getWithBufferImage(): ImageAndFilename = {
+    if (bufferImage != null) {
+      this
+    } else if (image != null) {
+      val bufferImageNew = LoadJFxImage.jFxImage2BufferImage(image)
+      this.copy(bufferImage = bufferImageNew)
+    } else {
+      println(s"Not good image and bufferImage both missing. Return null")
+      null
+    }
+  }
+
+  def getImage(): ImageAndFilename = {
+    if (image != null) {
+      this
+    } else if (bufferImage != null) {
+      val imageNew = LoadJFxImage.bufferImage2jFxImage(bufferImage.asInstanceOf[BufferImage[Byte]])
+      this.copy(image = imageNew)
+    } else {
+      println(s"Not good image and bufferImage both missing. Return null")
+      null
+    }
+  }
+}
