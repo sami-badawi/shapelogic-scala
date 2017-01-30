df3f56412a3c736532a794fe2202bc8c8076ff01
Sami Badawi <sami@citymaps.com>
2017-01-30 06:24:43 -0500

    Add IPolygon2D trait

--- src/main/scala/org/shapelogic/sc/polygon/Polygon.scala
+++ src/main/scala/org/shapelogic/sc/polygon/Polygon.scala
@@ -3,12 +3,14 @@ package org.shapelogic.sc.polygon
 import scala.collection.mutable.Set
 import scala.collection.mutable.Map
 import scala.collection.mutable.ArrayBuffer
+import org.shapelogic.sc.calculation.CalcInvoke
 
 object Polygon {
   val MAX_DISTANCE_BETWEEN_CLUSTER_POINTS: Double = 2;
 
 }
-class Polygon {
+class Polygon(annotatedShape: AnnotatedShapeImplementation) extends BaseAnnotatedShape(null)
+    with IPolygon2D with CalcInvoke[Polygon] with Cloneable with PointReplacable[Polygon] {
   import Polygon._
   var _bBox = new BBox()
   var _lines: Set[CLine] = Set()
@@ -17,73 +19,70 @@ class Polygon {
   protected var _aspectRatio: Double = 0
   protected var _closed: Boolean = false
   protected var _endPointCount: Int = -1;
-  protected val _pointsCountMap = Map[IPoint2D, Int]()
+  protected var _pointsCountMap = Map[IPoint2D, Integer]()
   protected val _pointsToLineMap = Map[IPoint2D, Set[CLine]]()
   protected var _version: Int = 0
   protected var _currentMultiLine: MultiLine = null
   protected val _endPointsClusters = ArrayBuffer[Set[IPoint2D]]()
   //I could make this lazy
-  protected var _annotatedShape: AnnotatedShapeImplementation = null
+  var _annotatedShape: AnnotatedShapeImplementation = null
   //  	protected val _polygonImprovers: ArrayBuffer[Improver[Polygon]] //XXX takes more imports
   protected var _perimeter: Double = 0
 
-  //	public Polygon() {
-  //		this(null);
-  //	}
-  //	
-  //	public Polygon(AnnotatedShapeImplementation annotatedShape) {
-  //		super(annotatedShape);
-  //		setup();
-  //		internalFactory();
-  //	}
-  //	
-  //	/** All the objects that needs special version should be created here. */
-  //	protected void internalFactory() {
-  //		_polygonImprovers = new ArrayList<Improver<Polygon> >();
-  //		_polygonImprovers.add(new FilterPolygonForSmallLines());
-  //		_polygonImprovers.add(new PolygonAnnotator());
-  //	}
-  //	
-  //	@Override
-  //	public BBox getBBox() {
-  //		getValue();
-  //		return _bBox;
-  //	}
-  //
-  //	@Override
-  //	public Set<CLine> getLines() {
-  //		return _lines;
-  //	}
-  //
-  //	@Override
-  //	public Set<IPoint2D> getPoints() {
-  //		return _points;
-  //	}
-  //
-  //	@Override
-  //	public Double getAspectRatio() {
-  //		getValue();
-  //        if (_bBox.minVal!=null) { 
-  //	        Double lenX = _bBox.maxVal.getX() - _bBox.minVal.getX();
-  //	        Double lenY = _bBox.maxVal.getY() - _bBox.minVal.getY();
-  //	        if (lenX > 0)
-  //	            _aspectRatio = lenY / lenX;
-  //	        else
-  //	            _aspectRatio = Double.POSITIVE_INFINITY;
-  //        }
-  //		return _aspectRatio;
-  //	}
-  //
-  //	@Override
-  //	/** Does this make sense for a polygon or only for multi line */
-  //	public Boolean isClosed() {
-  //		return _closed;
-  //	}
-  //
-  //	@Override
-  //	public Boolean isDirty() {
-  //		return _dirty;
-  //	}
+  def this() {
+    this(null);
+  }
+
+  /**
+   * Was constructor
+   */
+  def init() {
+    //  		super(annotatedShape)
+    setup();
+    //    internalFactory();
+  }
+
+  //  /** All the objects that needs special version should be created here. */
+  //  protected def internalFactory() = {
+  //    _polygonImprovers = new ArrayList < Improver < Polygon > >();
+  //    _polygonImprovers.add(new FilterPolygonForSmallLines());
+  //    _polygonImprovers.add(new PolygonAnnotator());
+  //  }
+
+  override def getBBox(): BBox = {
+    getValue();
+    return _bBox;
+  }
+
+  override def getLines(): Set[CLine] = {
+    return _lines;
+  }
+
+  override def getPoints(): Set[IPoint2D] = {
+    return _points;
+  }
+
+  override def getAspectRatio(): Double = {
+    getValue();
+    if (_bBox.minVal != null) {
+      val lenX: Double = _bBox.maxVal.getX() - _bBox.minVal.getX();
+      val lenY: Double = _bBox.maxVal.getY() - _bBox.minVal.getY();
+      if (lenX > 0)
+        _aspectRatio = lenY / lenX;
+      else
+        _aspectRatio = Double.PositiveInfinity;
+    }
+    return _aspectRatio;
+  }
+
+  /** Does this make sense for a polygon or only for multi line */
+  override def isClosed(): Boolean = {
+    return _closed;
+  }
+
+  override def isDirty(): Boolean = {
+    return _dirty;
+  }
   //
   //	@Override
   //	public void setup() {
@@ -92,55 +91,58 @@ class Polygon {
   //		_bBox = new BBox();
   //	}
   //
-  //    public Boolean containsPoint(IPoint2D point) {
-  //        return _points.contains(point);
-  //    }
-  //
-  //    public Boolean containsLine(ILine2D line) {
-  //        return _lines.contains(line);
-  //    }
-  //
-  //    private void addPoint(IPoint2D point) {
-  //        if (!containsPoint(point))
-  //            _points.add(point);
-  //    }
-  //    
-  //    /** this should not be used use addIndependentLine() instead */
-  //    public CLine addLine(IPoint2D point1, IPoint2D point2) {
-  //        CLine line = CLine.makeUnordered(point1, point2);
-  //        if (!containsLine(line))
-  //            _lines.add(line);
-  //        addPoint(point1);
-  //        addPoint(point2);
-  //        return line;
-  //    }
-  //
-  //    /** this should not be used use addIndependentLine() instead */
-  //    @Deprecated
-  //    protected CLine addLine(CLine line) {
-  //        if (!containsLine(line)) {
-  //            _lines.add(line);
-  //            addPoint(line.getStart());
-  //            addPoint(line.getEnd());
-  //        }
-  //        return line;
-  //    }
-  //    
-  //	@Override
-  //	public Polygon invoke() {
-  //        _bBox = findBbox();
-  //        findPointCount();
-  //        _dirty = false;
-  //        return this;
-  //    }
-  //
-  //    private BBox findBbox() {
-  //    	if(_bBox == null)
-  //    		_bBox = new BBox();
-  //        for (IPoint2D pointInPolygon : _points )
-  //            _bBox.addPoint(pointInPolygon);
-  //        return _bBox;
-  //    }
+  def containsPoint(point: IPoint2D): Boolean = {
+    return _points.contains(point);
+  }
+
+  def containsLine(line: ILine2D): Boolean = {
+    return _lines.contains(line.asInstanceOf[CLine]) //XXX 
+  }
+
+  def addPoint(point: IPoint2D): Unit = {
+    if (!containsPoint(point))
+      _points.add(point);
+  }
+
+  /** this should not be used use addIndependentLine() instead */
+  def addLine(point1: IPoint2D, point2: IPoint2D): CLine = {
+    val line: CLine = CLine.makeUnordered(point1, point2)
+    if (!containsLine(line))
+      _lines.add(line);
+    addPoint(point1);
+    addPoint(point2);
+    return line;
+  }
+
+  /**
+   * this should not be used use addIndependentLine() instead
+   * @Deprecated
+   *
+   */
+  def addLine(line: CLine): CLine = {
+    if (!containsLine(line)) {
+      _lines.add(line);
+      addPoint(line.getStart());
+      addPoint(line.getEnd());
+    }
+    return line;
+  }
+
+  override def invoke(): Polygon = {
+    _bBox = findBbox();
+    findPointCount();
+    _dirty = false;
+    return this;
+  }
+
+  def findBbox(): BBox = {
+    if (_bBox == null)
+      _bBox = new BBox();
+    _points.foreach { (pointInPolygon: IPoint2D) =>
+      _bBox.addPoint(pointInPolygon)
+    }
+    return _bBox;
+  }
   //    
   //    /** Return a cleaned up polygon 
   //     * 
@@ -196,12 +198,11 @@ class Polygon {
   //    	return result;
   //    }
   //    
-  //	@Override
-  //	public Polygon getValue() {
-  //		if (isDirty())
-  //			invoke();
-  //		return this;
-  //	}
+  override def getValue(): Polygon = {
+    if (isDirty())
+      invoke();
+    return this;
+  }
   //	
   //	public List<CLine> getVerticalLines() {
   //		return CLine.filterVertical(_lines);
@@ -211,30 +212,32 @@ class Polygon {
   //		return CLine.filterHorizontal(_lines);
   //	}
   //	
-  //	/** Find how many lines each point is part of by making a map */
-  //	public Map<IPoint2D,Integer> getPointsCountMap() {
-  //		if (_pointsCountMap == null) {
-  //			_pointsCountMap = new TreeMap<IPoint2D,Integer>();
-  //			for (CLine line: _lines) {
-  //				Integer startCount = _pointsCountMap.get(line.getStart());
-  //				if (startCount == null)
-  //					startCount = 1;
-  //				else
-  //					startCount++;
-  //				_pointsCountMap.put(line.getStart(),startCount);
-  //				if (line.isPoint())
-  //					continue;
-  //				Integer endCount = _pointsCountMap.get(line.getEnd());
-  //				if (endCount == null)
-  //					endCount = 1;
-  //				else
-  //					endCount++;
-  //				_pointsCountMap.put(line.getEnd(),endCount);
-  //			}
-  //		}
-  //		return _pointsCountMap;
-  //	}
-  //	
+  /** Find how many lines each point is part of by making a map */
+  def getPointsCountMap(): Map[IPoint2D, Integer] = {
+    if (_pointsCountMap == null) {
+      _pointsCountMap = Map[IPoint2D, Integer]()
+      _lines.foreach { (line: CLine) =>
+        {
+          var startCount: Integer = _pointsCountMap.getOrElse(line.getStart(), null)
+          if (startCount == null)
+            startCount = 1;
+          else
+            startCount = startCount + 1
+          _pointsCountMap.put(line.getStart(), startCount);
+          if (!line.isPoint()) {
+            var endCount: Integer = _pointsCountMap.getOrElse(line.getEnd(), null)
+            if (endCount == null)
+              endCount = 1;
+            else
+              endCount = endCount + 1
+            _pointsCountMap.put(line.getEnd(), endCount);
+          }
+        }
+      }
+    }
+    return _pointsCountMap;
+  }
+
   //	/** Find how many lines each point is part of by making a map */
   //	public Map<IPoint2D,Set<CLine>> getPointsToLineMap() {
   //		if (_pointsToLineMap == null) {
@@ -257,22 +260,24 @@ class Polygon {
   //		return _pointsToLineMap;
   //	}
   //	
-  //	public Int findPointCount() {
-  //		getPointsCountMap();
-  //		Integer ONE = 1;
-  //		_endPointCount = 0;
-  //		for (IPoint2D point: _pointsCountMap.keySet()) {
-  //			if (ONE.equals(_pointsCountMap.get(point) ))
-  //				_endPointCount++;
-  //		}
-  //		return _endPointCount;
-  //	}
-  //
-  //	public Int getEndPointCount() {
-  //		getValue();
-  //		return _endPointCount;
-  //	}
-  //	
+  def findPointCount(): Int = {
+    getPointsCountMap();
+    val ONE: Integer = 1;
+    _endPointCount = 0;
+    _pointsCountMap.keys.toSeq.foreach { point =>
+      {
+        if (ONE.equals(_pointsCountMap.get(point)))
+          _endPointCount = _endPointCount + 1
+      }
+    }
+    return _endPointCount;
+  }
+
+  def getEndPointCount(): Int = {
+    getValue();
+    return _endPointCount;
+  }
+
   //	public Collection<CLine> getLinesForPoint(IPoint2D point) {
   //		TreeSet<CLine> result = new TreeSet<CLine>();  
   //		if (point == null)
@@ -287,14 +292,14 @@ class Polygon {
   //		return result;
   //	}
   //
-  //	public Int getVersion() {
-  //		return _version;
-  //	}
-  //
-  //	public void setVersion(Int version) {
-  //		_version = version;
-  //	}
-  //
+  def getVersion(): Int = {
+    return _version;
+  }
+
+  def setVersion(version: Int) = {
+    _version = version;
+  }
+
   //	public void startMultiLine() {
   //		_currentMultiLine = new MultiLine(this.getAnnotatedShape());
   //	}
@@ -372,52 +377,56 @@ class Polygon {
   //		}
   //	}
   //	
-  //	/** No filtering in first version */
-  //	@Override
-  //	public Polygon replacePointsInMap(Map<IPoint2D,IPoint2D> pointReplacementMap,
-  //			AnnotatedShapeImplementation annotatedShape) {
-  //		Polygon replacedPolygon = new Polygon(annotatedShape);
-  //		replacedPolygon.setup();
-  //	    for (CLine line : _lines) {
-  //	    	CLine newLine = line.replacePointsInMap(pointReplacementMap,annotatedShape);
-  //			if (!newLine.isPoint()) {
-  //				replacedPolygon.addLine(newLine);
-  //			}
-  //		}
-  //		Set<Object> annotationForOldPolygon = null; 
-  //		if (annotatedShape != null)
-  //			annotationForOldPolygon = annotatedShape.getAnnotationForShapes(this);
-  //		if (annotationForOldPolygon != null) {
-  //			annotatedShape.putAllAnnotation(replacedPolygon, annotationForOldPolygon); 
-  //		}
-  //		return replacedPolygon;
-  //	}
-  //
-  //	@Override
-  //	public IPoint2D getCenter() {
-  //		return _bBox.getCenter();
-  //	}
-  //
-  //	@Override
-  //	public Double getDiameter() {
-  //		return getBBox().getDiameter();
-  //	}
-  //	
+  /** No filtering in first version */
+
+  //override 
+  def replacePointsInMap(pointReplacementMap: Map[IPoint2D, IPoint2D],
+    annotatedShape: AnnotatedShapeImplementation): Polygon = {
+    val replacedPolygon: Polygon = new Polygon(annotatedShape);
+    replacedPolygon.setup();
+    _lines.foreach {
+      (line: CLine) =>
+        {
+          val newLine = line.replacePointsInMap(pointReplacementMap, annotatedShape);
+          if (!newLine.isPoint()) {
+            replacedPolygon.addIndependentLine(newLine);
+          }
+        }
+    }
+    var annotationForOldPolygon: Set[Object] = null;
+    if (annotatedShape != null)
+      annotationForOldPolygon = annotatedShape.getAnnotationForShapes(this);
+    if (annotationForOldPolygon != null) {
+      annotatedShape.putAllAnnotation(replacedPolygon, annotationForOldPolygon);
+    }
+    return replacedPolygon;
+  }
+
+  override def getCenter(): IPoint2D = {
+    return _bBox.getCenter();
+  }
+
+  override def getDiameter(): Double = {
+    return getBBox().getDiameter();
+  }
+
   //	public void setPolygonImprovers(List<Improver<Polygon>> improvers) {
   //		_polygonImprovers = improvers;
   //	}
   //
-  //	/** To have the same interface as MultiLinePolygon */
-  //	public CLine addIndependentLine(IPoint2D point1, IPoint2D point2) {
-  //		return addLine(point1, point2);
-  //	}
-  //
-  //    /** Most of the time this should not be used use the version taking input 
-  //     * points instead */
-  //    protected CLine addIndependentLine(CLine line) {
-  //        return addLine(line);
-  //    }
-  //    
+  /** To have the same interface as MultiLinePolygon */
+  def addIndependentLine(point1: IPoint2D, point2: IPoint2D): CLine = {
+    return addLine(point1, point2);
+  }
+
+  /**
+   * Most of the time this should not be used use the version taking input
+   * points instead
+   */
+  def addIndependentLine(line: CLine): CLine = {
+    return addLine(line)
+  }
+
   //	/** To have the same interface as MultiLinePolygon */
   //	public Set<CLine> getIndependentLines() {
   //		return getLines();
@@ -442,9 +451,9 @@ class Polygon {
   //		return filterObject.filter();
   //	}
 
-  //	public Int getHoleCount() {
-  //		return getLines().size() + 1 - getPoints().size();
-  //	}
+  def getHoleCount(): Int = {
+    return getLines().size + 1 - getPoints().size
+  }
 
   //	@Override
   //	public String toString() {
@@ -482,12 +491,12 @@ class Polygon {
   //		return result.toString();
   //    }
 
-  //	public Double getPerimeter() {
-  //		return _perimeter;
-  //	}
+  def getPerimeter(): Double = {
+    return _perimeter;
+  }
 
-  //	public void setPerimeter(Double perimeter) {
-  //		_perimeter = perimeter;
-  //	}
+  def setPerimeter(perimeter: Double) {
+    _perimeter = perimeter;
+  }
 
 }
