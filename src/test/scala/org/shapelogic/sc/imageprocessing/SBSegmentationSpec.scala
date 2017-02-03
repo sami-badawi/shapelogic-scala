a18d896ce5f1e71444ced6c1eb1093abaf4020d3
Sami Badawi <sami@citymaps.com>
2017-02-02 20:30:31 -0500

    First segmentation unit test

--- src/test/scala/org/shapelogic/sc/imageprocessing/SBSegmentationSpec.scala
+++ src/test/scala/org/shapelogic/sc/imageprocessing/SBSegmentationSpec.scala
@@ -8,9 +8,14 @@ import spire.std._
 import spire.syntax.ring._
 //import spire.math.Numberic
 import spire.implicits._
+import org.shapelogic.sc.image.BufferImage
 
 class SBSegmentationSpec extends FunSuite with BeforeAndAfterEach {
   test("") {
-    assertResult(1) { 1 }
+    val bytes = Array[Byte](100)
+    val inputImage = new BufferImage[Byte](1, 1, 1, bytes)
+    val segmentation = new SBSegmentation(inputImage, None)
+    val outputBufferImage = segmentation.result
+    assertResult(bytes.toSeq) { outputBufferImage.data.toSeq }
   }
 }
