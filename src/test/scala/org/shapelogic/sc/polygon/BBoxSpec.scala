81880f632c3953a8bf7f44c8d32090d5eec37993
Sami Badawi <sami@citymaps.com>
2017-02-04 08:02:45 -0500

    Uncomment and fix unit tests for BBox

--- src/test/scala/org/shapelogic/sc/polygon/BBoxSpec.scala
+++ src/test/scala/org/shapelogic/sc/polygon/BBoxSpec.scala
@@ -6,37 +6,47 @@ import scala.specialized
 import spire.math.Numeric
 
 class BBoxSpec extends FunSuite with BeforeAndAfterEach {
-//  import BBox._
-//
-//    val north: IPoint2D = new CPointDouble(110.,100.);
-//    val south: IPoint2D = new CPointDouble(110.,120.);
-//    val east: IPoint2D = new CPointDouble(120.,110.);
-//    val west: IPoint2D = new CPointDouble(100.,110.);
-//    val center: IPoint2D = new CPointDouble(110.,110.);
-//    val eMin: IPoint2D = new CPointDouble(100.,100.);
-//    val eMax: IPoint2D = new CPointDouble(120.,120.);
-//    
-//  test("Check testPolygon") {
-//    	val poly1 = new Polygon();
-//        assertEquals("point size should be 0", 0, poly1.getPoints().size());
-//        poly1.addLine(north,south);
-//        assertEquals("point size should be 2", 2, poly1.getPoints().size());
-//        poly1.addLine(east,west);
-//        assertEquals("Fist point should be west ", poly1.getPoints().iterator().next(), west);
-//        poly1.invoke();
-//        val bbox: BBox = poly1.getBBox();
-//        assertEquals("minVal wrong", bbox.minVal, eMin);
-//        assertEquals("maxVal wrong", bbox.maxVal, eMax);
-//    }
-//    
-//  test("Check testDiagonal") {
-//    	val poly1 = new Polygon();
-//        poly1.addLine(north,south);
-//        poly1.addLine(east,west);
-//        poly1.invoke();
-//        val bbox: BBox = poly1.getBBox();
-//        assertEquals("center wrong", eMin, bbox.getDiagonalVector(0.0));
-//        assertEquals("center wrong", center, bbox.getDiagonalVector(0.5));
-//        assertEquals("center wrong", eMax, bbox.getDiagonalVector(1.));
-//    }  
+  //  import BBox._
+  //
+  val north: IPoint2D = new CPointDouble(110.0, 100.0)
+  val south: IPoint2D = new CPointDouble(110.0, 120.0)
+  val east: IPoint2D = new CPointDouble(120.0, 110.0)
+  val west: IPoint2D = new CPointDouble(100.0, 110.0)
+  val center: IPoint2D = new CPointDouble(110.0, 110.0)
+  val eMin: IPoint2D = new CPointDouble(100.0, 100.0)
+  val eMax: IPoint2D = new CPointDouble(120.0, 120.0)
+
+  test("Check testPolygon") {
+    val poly1 = new Polygon();
+    //    "point size should be 0",
+    assertResult(0) { poly1.getPoints().size }
+    poly1.addLine(north, south);
+    //    "point size should be 2", 
+    assertResult(2) { poly1.getPoints().size }
+    poly1.addLine(east, west)
+    //    "Fist point should be west ", 
+    assertResult(poly1.getPoints().iterator.next) { north }
+    poly1.invoke();
+    val bbox: BBox = poly1.getBBox();
+    //    "minVal wrong", 
+    assertResult(bbox.minVal) { eMin }
+    //    "maxVal wrong", 
+    assertResult(bbox.maxVal) { eMax }
+  }
+
+  test("Check testDiagonal") {
+    val poly1 = new Polygon()
+    poly1.addLine(north, south)
+    poly1.addLine(east, west)
+    poly1.invoke();
+    val bbox: BBox = poly1.getBBox()
+    //"center wrong",
+    assertResult(eMin) { bbox.getDiagonalVector(0.0) }
+
+    //"center wrong", 
+    assertResult(center) { bbox.getDiagonalVector(0.5) }
+
+    //"center wrong",
+    assertResult(eMax) { bbox.getDiagonalVector(1.0) }
+  }
 }
