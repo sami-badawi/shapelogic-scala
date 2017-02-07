ef2f9613e1426e6cee3c3fd8476265689c35efaf
Sami Badawi <sami@citymaps.com>
2017-02-06 22:43:45 -0500

    Clean up

--- src/main/scala/org/shapelogic/sc/imageprocessing/EdgeTracer.scala
+++ src/main/scala/org/shapelogic/sc/imageprocessing/EdgeTracer.scala
@@ -6,11 +6,11 @@ import org.shapelogic.sc.util.Constants.RIGHT
 import org.shapelogic.sc.util.Constants.UP
 
 //import org.shapelogic.sc.color.ColorFactory
-//import org.shapelogic.sc.color.IColorDistanceWithImage;
-//import org.shapelogic.sc.imageutil.SLImage;
-import org.shapelogic.sc.polygon.CPointInt;
-import org.shapelogic.sc.polygon.Polygon;
-import org.shapelogic.sc.util.Constants;
+//import org.shapelogic.sc.color.IColorDistanceWithImage
+//import org.shapelogic.sc.imageutil.SLImage
+import org.shapelogic.sc.polygon.CPointInt
+import org.shapelogic.sc.polygon.Polygon
+import org.shapelogic.sc.util.Constants
 import org.shapelogic.sc.image.BufferImage
 
 import spire.implicits._
@@ -36,13 +36,13 @@ class EdgeTracer(image: BufferImage[Byte], maxDistance: Double, traceCloseToColo
   lazy val height: Int = image.height
 
   var _dirs = new Array[Boolean](Constants.DIRECTIONS_AROUND_POINT)
-  val STEP_SIZE_FOR_4_DIRECTIONS = 2;
+  val STEP_SIZE_FOR_4_DIRECTIONS = 2
   //	
   //	/** Constructs a Wand object from an ImageProcessor. */
   //	public EdgeTracer(SLImage image, Int referenceColor, double maxDistance, Boolean traceCloseToColor) {
-  //		_colorDistanceWithImage = ColorFactory.makeColorDistanceWithImage(image);
-  //		_colorDistanceWithImage.setReferenceColor(referenceColor);
-  //		_traceCloseToColor = traceCloseToColor;
+  //		_colorDistanceWithImage = ColorFactory.makeColorDistanceWithImage(image)
+  //		_colorDistanceWithImage.setReferenceColor(referenceColor)
+  //		_traceCloseToColor = traceCloseToColor
   //	}
   //	
 
@@ -54,7 +54,7 @@ class EdgeTracer(image: BufferImage[Byte], maxDistance: Double, traceCloseToColo
       return false
     if (width <= x || height <= y)
       return false
-    return traceCloseToColor ^ (maxDistance < _colorDistanceWithImage.distanceToReferenceColor(x, y));
+    traceCloseToColor ^ (maxDistance < _colorDistanceWithImage.distanceToReferenceColor(x, y))
   }
 
   /**
@@ -69,54 +69,53 @@ class EdgeTracer(image: BufferImage[Byte], maxDistance: Double, traceCloseToColo
     //Find top point inside
     do {
       y -= 1
-    } while (inside(x, y));
+    } while (inside(x, y))
     y += 1
     //Find leftmost top point inside
     do {
       x -= 1
-    } while (inside(x, y));
+    } while (inside(x, y))
     x += 1
-    return traceEdge(x, y, 2)
+    traceEdge(x, y, 2)
   }
 
   def nextDirection(x: Int, y: Int, lastDirection: Int, clockwise: Boolean): Int = {
-    var directions: Array[Boolean] = makeDirections(x, y, true);
-    val lastDirectionReleativeCurrent = lastDirection + Constants.DIRECTIONS_AROUND_POINT / 2;
+    var directions: Array[Boolean] = makeDirections(x, y, true)
+    val lastDirectionReleativeCurrent = lastDirection + Constants.DIRECTIONS_AROUND_POINT / 2
     val stepSize = STEP_SIZE_FOR_4_DIRECTIONS
     cfor(2)(_ <= Constants.DIRECTIONS_AROUND_POINT, _ + stepSize) { i =>
       var step = i
       if (!clockwise)
-        step = Constants.DIRECTIONS_AROUND_POINT - i;
-      val real_direction = (lastDirectionReleativeCurrent + step) % Constants.DIRECTIONS_AROUND_POINT;
+        step = Constants.DIRECTIONS_AROUND_POINT - i
+      val real_direction = (lastDirectionReleativeCurrent + step) % Constants.DIRECTIONS_AROUND_POINT
       //Return first point that is inside
       if (directions(real_direction))
-        return real_direction;
+        return real_direction
     }
-    return -1; //Not found
+    -1 //Not found
   }
 
   def makeDirections(x: Int, y: Int, only4points: Boolean): Array[Boolean] = {
-    var stepSize = 1;
+    var stepSize = 1
     if (only4points)
-      stepSize = STEP_SIZE_FOR_4_DIRECTIONS;
+      stepSize = STEP_SIZE_FOR_4_DIRECTIONS
     cfor(0)(_ < Constants.DIRECTIONS_AROUND_POINT, _ + stepSize) { i =>
-      _dirs(i) = inside(x + Constants.CYCLE_POINTS_X(i), y + Constants.CYCLE_POINTS_Y(i));
+      _dirs(i) = inside(x + Constants.CYCLE_POINTS_X(i), y + Constants.CYCLE_POINTS_Y(i))
     }
-    return _dirs;
+    _dirs
   }
 
   def traceEdge(xstart: Int, ystart: Int, startingDirection: Int): Polygon = { //XXX
-    val polygon = new Polygon();
-    polygon.startMultiLine();
-    var chainCodeHandler = new ChainCodeHandler(polygon.getAnnotatedShape());
-    chainCodeHandler = new ChainCodeHandler(polygon.getAnnotatedShape());
+    val polygon = new Polygon()
+    polygon.startMultiLine()
+    val chainCodeHandler = new ChainCodeHandler(polygon.getAnnotatedShape())
     chainCodeHandler.setup()
     chainCodeHandler.setMultiLine(polygon.getCurrentMultiLine())
-    chainCodeHandler.setFirstPoint(new CPointInt(xstart, ystart));
+    chainCodeHandler.setFirstPoint(new CPointInt(xstart, ystart))
     var x = xstart
     var y = ystart
     var direction: Int = BaseVectorizer.oppesiteDirection(nextDirection(x, y, startingDirection - 2, false).toByte)
-    var count = 0;
+    var count = 0
     var stop = false
     do {
       count += 1
@@ -125,16 +124,16 @@ class EdgeTracer(image: BufferImage[Byte], maxDistance: Double, traceCloseToColo
         stop = true
       direction match {
         case UP => {
-          y = y - 1;
+          y = y - 1
         }
         case DOWN => {
-          y = y + 1;
+          y = y + 1
         }
         case LEFT => {
-          x = x - 1;
+          x = x - 1
         }
         case RIGHT => {
-          x = x + 1;
+          x = x + 1
         }
         case -1 => {
           stop = true
@@ -143,13 +142,13 @@ class EdgeTracer(image: BufferImage[Byte], maxDistance: Double, traceCloseToColo
       //If the chain becomes too long just give up
       if (!chainCodeHandler.addChainCode(direction.toByte))
         stop = true
-      //		} while ((x!=xstart || y!=ystart));
+      //		} while ((x!=xstart || y!=ystart))
       //Original clause causes termination problems
-    } while ((x != xstart || y != ystart || direction != startingDirection) || stop);
-    chainCodeHandler.getValue();
-    polygon.setPerimeter(chainCodeHandler.getPerimeter());
-    polygon.getValue();
-    polygon.getBBox().add(chainCodeHandler._bBox);
-    return polygon;
+    } while ((x != xstart || y != ystart || direction != startingDirection) || stop)
+    chainCodeHandler.getValue()
+    polygon.setPerimeter(chainCodeHandler.getPerimeter())
+    polygon.getValue()
+    polygon.getBBox().add(chainCodeHandler._bBox)
+    polygon
   }
 }
