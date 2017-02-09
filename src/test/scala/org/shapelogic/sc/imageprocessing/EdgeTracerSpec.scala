d1d038f9e929666b9cee8b7171c7c8e847040013
Sami Badawi <sami@citymaps.com>
2017-02-09 00:43:22 -0500

    Enabled first test for EdgeTracer. It did not pass.

--- src/test/scala/org/shapelogic/sc/imageprocessing/EdgeTracerSpec.scala
+++ src/test/scala/org/shapelogic/sc/imageprocessing/EdgeTracerSpec.scala
@@ -8,6 +8,10 @@ import org.shapelogic.sc.util.PointType
 
 import org.scalatest._
 import org.shapelogic.sc.image.BufferImage
+import org.shapelogic.sc.javafx.LoadJFxImage
+import javafx.scene.image.Image
+import org.shapelogic.sc.javafx.LoadJFxImage
+import org.shapelogic.sc.javafx.LoadJFxImage
 
 object EdgeTracerSpec {
 
@@ -25,6 +29,11 @@ object EdgeTracerSpec {
   def filePath(dir: String, fileName: String, fileFormat: String): String = {
     return dir + "/" + fileName + fileFormat;
   }
+
+  def loadImage(filename: String): BufferImage[Byte] = {
+    LoadJFxImage.loadBufferImage(filename)
+  }
+
 }
 /**
  * Test of EdgeTracer.<br />
@@ -35,115 +44,115 @@ object EdgeTracerSpec {
 class EdgeTracerSpec extends FunSuite with BeforeAndAfterEach {
   import EdgeTracerSpec._
 
-//  val boxPerimeter: Double = 17.656854249492383; //
-//  val iPerimeter: Double = 54.0
-//
-//  /**
-//   *
-//   */
-//  def getInstance(
-//    imagePath: String,
-//    referenceColor: Array[Byte],
-//    maxDistance: Double,
-//    traceCloseToColor: Boolean): IEdgeTracer = {
-//    null
-//  }
-//
-//  test("Redbox") {
-//    val filename = "redbox";
-//    val image: BufferImage[Byte] = null // new IJImage(filePath(filename,".png"));
-//    val foregroundColor: Int = 0xff0000;
-//    val foregroundColorClose: Int = 0xfe0000;
-//    assert(image != null);
-//    //  		assert(!image.isEmpty());
-//    assertResult(10) { image.width }
-//    assertResult(10) { image.height }
-//    //    assert(image.isRgb())
-//    assertResult(-1) { image.getPixel(0, 0).toSeq } //Background unmasked
-//    //    assertResult(0xffffff, image.getPixel(0, 0) & 0xffffff); //Background
-//    assertResult(foregroundColor) { image.getPixel(2, 2) } //Foreground
-//    val edgeTracer: IEdgeTracer = getInstance(image, foregroundColorClose, 10, true)
-//    val cch: Polygon = edgeTracer.autoOutline(5, 2)
-//    //if 5,5 was used as start point a soft point would have been found
-//    assertResult(boxPerimeter) { cch.getPerimeter() }
-//    assertResult(4) { cch.getAnnotatedShape().getShapesForAnnotation(PointType.HARD_CORNER).size() }
-//    assertResult(4) { cch.getAnnotatedShape().getShapesForAnnotation(LineType.STRAIGHT).size() }
-//    printAnnotaions(cch.getAnnotatedShape());
-//  }
-//
-//  test("RedboxInverse") {
-//    String filename = "redbox";
-//    val image: BufferImage[Byte] = null //new IJImage(filePath(filename,".png"));
-//    int foregroundColor = 0xff0000;
-//    int backgroundColorClose = 0xfffeff;
-//    assertNotNull(image);
-//    assertFalse(image.isEmpty());
-//    assertResult(10, image.getWidth());
-//    assertResult(10, image.getHeight());
-//    assert(image.isRgb());
-//    assertResult(-1) { image.getPixel(0, 0) } //Background unmasked
-//    assertResult(0xffffff) { image.getPixel(0, 0) } //Background
-//    assertResult(foregroundColor) { image.getPixel(2, 2) } //Foreground
-//    IEdgeTracer edgeTracer = getInstance(image, backgroundColorClose, 10, false);
-//    Polygon cch = edgeTracer.autoOutline(5, 2);
-//    //if 5,5 was used as start point a soft point would have been found
-//    assertResult(boxPerimeter, cch.getPerimeter());
-//    assertResult(4) { cch.getAnnotatedShape().getShapesForAnnotation(PointType.HARD_CORNER).size() }
-//    assertResult(4) { cch.getAnnotatedShape().getShapesForAnnotation(LineType.STRAIGHT).size() }
-//    printAnnotaions(cch.getAnnotatedShape());
-//  }
-//
-//  test("Blackbox") {
-//    val filename = "blackbox";
-//    val image: BufferImage[Byte] = null // = new IJImage(filePath(filename));
-//    int foregroundColor = 255;
-//    assertNotNull(image);
-//    assertFalse(image.isEmpty());
-//    assertResult(10, image.getWidth());
-//    assertResult(10, image.getHeight());
-//    assert(image.isGray());
-//    assertResult(0, image.getPixel(0, 0)); //Background
-//    assertResult(foregroundColor, image.getPixel(2, 2)); //Foreground
-//    IEdgeTracer edgeTracer = getInstance(image, foregroundColor, 10, true);
-//    Polygon cch = edgeTracer.autoOutline(5, 5);
-//    assertClose(boxPerimeter, cch.getPerimeter());
-//  }
-//
-//  public void testI() {
-//    String filename = "I";
-//    val image: BufferImage[Byte] = null // new IJImage(filePath("./src/test/resources/images/smallThinLetters", filename, ".gif"));
-//    int foregroundColor = 255;
-//    assertNotNull(image);
-//    assertFalse(image.isEmpty());
-//    assertResult(30, image.getWidth());
-//    assertResult(30, image.getHeight());
-//    assert(image.isGray());
-//    assertResult(0, image.getPixel(0, 0)); //Background
-//    assertResult(foregroundColor, image.getPixel(14, 2)); //Foreground
-//    IEdgeTracer edgeTracer = getInstance(image, foregroundColor, 10, true);
-//    Polygon cch = edgeTracer.autoOutline(14, 2);
-//    assertResult(iPerimeter, cch.getPerimeter());
-//  }
-//
-//  def printAnnotaions(annotatedShape: AnnotatedShape): Unit = {
-//    println("Print annotations:");
-//    val map = annotatedShape.getMap(); //Map<Object, Set<GeometricShape2D>>
-//    map.foreach { (entry) =>
-//      System.out.println(entry.getKey() + ":\n" + entry.getValue());
-//    }
-//  }
-//
-//  test("NegativeModulus") {
-//    assertResult(-4) { -4 % 8 }
-//  }
-//
-//  def assertClose(expected: Double, actual: Double): Unit = {
-//    val absolute: Double = 1;
-//    val difference: Double = Math.abs(expected - actual);
-//    val far: Boolean = absolute < difference;
-//    if (far) {
-//      println("Expected: " + expected + " not close to actual: " + actual)
-//      assert(false);
-//    }
-//  }
+  val boxPerimeter: Double = 17.656854249492383; //
+  val iPerimeter: Double = 54.0
+
+  def getInstance(
+    imagePath: String,
+    referenceColor: Array[Byte],
+    maxDistance: Double,
+    traceCloseToColor: Boolean): IEdgeTracer = {
+    val image = LoadJFxImage.loadBufferImage(imagePath)
+    val edgeTracer = new EdgeTracer(image, maxDistance, traceCloseToColor)
+    edgeTracer.setReferencePointArray(referenceColor)
+    edgeTracer
+  }
+
+  test("Redbox") {
+    val filename = "redbox";
+    val image: BufferImage[Byte] = loadImage(filePath(filename, ".png"));
+    val foregroundColor: Int = 0xff0000;
+    val foregroundColorClose: Array[Byte] = Array(0, 0, 254.toByte, -1) // 0xfe0000;
+    assert(image != null);
+    //  		assert(!image.isEmpty());
+    assertResult(10) { image.width }
+    assertResult(10) { image.height }
+    //    assert(image.isRgb())
+    assertResult(-1) { image.getPixel(0, 0).toSeq } //Background unmasked
+    //    assertResult(0xffffff, image.getPixel(0, 0) & 0xffffff); //Background
+    assertResult(foregroundColor) { image.getPixel(2, 2) } //Foreground
+    val edgeTracer: IEdgeTracer = EdgeTracer.fromBufferImage(image, foregroundColorClose, 10, true)
+    val cch: Polygon = edgeTracer.autoOutline(5, 2)
+    //if 5,5 was used as start point a soft point would have been found
+    assertResult(boxPerimeter) { cch.getPerimeter() }
+    //    assertResult(4) { cch.getAnnotatedShape().getShapesForAnnotation(PointType.HARD_CORNER).size() }
+    //    assertResult(4) { cch.getAnnotatedShape().getShapesForAnnotation(LineType.STRAIGHT).size() }
+    //    printAnnotaions(cch.getAnnotatedShape());
+  }
+
+  //  test("RedboxInverse") {
+  //    String filename = "redbox";
+  //    val image: BufferImage[Byte] = null //loadImage(filePath(filename,".png"));
+  //    int foregroundColor = 0xff0000;
+  //    int backgroundColorClose = 0xfffeff;
+  //    assertNotNull(image);
+  //    assertFalse(image.isEmpty());
+  //    assertResult(10, image.getWidth());
+  //    assertResult(10, image.getHeight());
+  //    assert(image.isRgb());
+  //    assertResult(-1) { image.getPixel(0, 0) } //Background unmasked
+  //    assertResult(0xffffff) { image.getPixel(0, 0) } //Background
+  //    assertResult(foregroundColor) { image.getPixel(2, 2) } //Foreground
+  //    IEdgeTracer edgeTracer = getInstance(image, backgroundColorClose, 10, false);
+  //    Polygon cch = edgeTracer.autoOutline(5, 2);
+  //    //if 5,5 was used as start point a soft point would have been found
+  //    assertResult(boxPerimeter, cch.getPerimeter());
+  //    assertResult(4) { cch.getAnnotatedShape().getShapesForAnnotation(PointType.HARD_CORNER).size() }
+  //    assertResult(4) { cch.getAnnotatedShape().getShapesForAnnotation(LineType.STRAIGHT).size() }
+  //    printAnnotaions(cch.getAnnotatedShape());
+  //  }
+  //
+  //  test("Blackbox") {
+  //    val filename = "blackbox";
+  //    val image: BufferImage[Byte] = null // = loadImage(filePath(filename));
+  //    int foregroundColor = 255;
+  //    assertNotNull(image);
+  //    assertFalse(image.isEmpty());
+  //    assertResult(10, image.getWidth());
+  //    assertResult(10, image.getHeight());
+  //    assert(image.isGray());
+  //    assertResult(0, image.getPixel(0, 0)); //Background
+  //    assertResult(foregroundColor, image.getPixel(2, 2)); //Foreground
+  //    IEdgeTracer edgeTracer = getInstance(image, foregroundColor, 10, true);
+  //    Polygon cch = edgeTracer.autoOutline(5, 5);
+  //    assertClose(boxPerimeter, cch.getPerimeter());
+  //  }
+  //
+  //  public void testI() {
+  //    String filename = "I";
+  //    val image: BufferImage[Byte] = null // loadImage(filePath("./src/test/resources/images/smallThinLetters", filename, ".gif"));
+  //    int foregroundColor = 255;
+  //    assertNotNull(image);
+  //    assertFalse(image.isEmpty());
+  //    assertResult(30, image.getWidth());
+  //    assertResult(30, image.getHeight());
+  //    assert(image.isGray());
+  //    assertResult(0, image.getPixel(0, 0)); //Background
+  //    assertResult(foregroundColor, image.getPixel(14, 2)); //Foreground
+  //    IEdgeTracer edgeTracer = getInstance(image, foregroundColor, 10, true);
+  //    Polygon cch = edgeTracer.autoOutline(14, 2);
+  //    assertResult(iPerimeter, cch.getPerimeter());
+  //  }
+  //
+  //  def printAnnotaions(annotatedShape: AnnotatedShape): Unit = {
+  //    println("Print annotations:");
+  //    val map = annotatedShape.getMap(); //Map<Object, Set<GeometricShape2D>>
+  //    map.foreach { (entry) =>
+  //      System.out.println(entry.getKey() + ":\n" + entry.getValue());
+  //    }
+  //  }
+  //
+  //  test("NegativeModulus") {
+  //    assertResult(-4) { -4 % 8 }
+  //  }
+  //
+  //  def assertClose(expected: Double, actual: Double): Unit = {
+  //    val absolute: Double = 1;
+  //    val difference: Double = Math.abs(expected - actual);
+  //    val far: Boolean = absolute < difference;
+  //    if (far) {
+  //      println("Expected: " + expected + " not close to actual: " + actual)
+  //      assert(false);
+  //    }
+  //  }
 }
