19226a75d5fff3ed9645210da9f72efa8df5536f
Sami Badawi <sami@citymaps.com>
2017-02-09 22:12:29 -0500

    Just a little more cleanup

---
+++ src/main/scala/org/shapelogic/sc/calculation/calculation.scala
@@ -0,0 +1,13 @@
+package org.shapelogic.sc
+
+/**
+ * A big part of ShapeLogic Java was to be a library for functional programming
+ * and lazy streams in Java at a point where this was not present
+ *
+ * The package org.shapelogic.sc.calculation contains a lot of this code.
+ * There are better ways to do this in Scala
+ * So this whole package will probably be phased out
+ */
+package object calculation {
+
+}
