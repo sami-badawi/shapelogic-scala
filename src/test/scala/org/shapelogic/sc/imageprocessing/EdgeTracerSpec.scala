bb7a7c00f49c285d8c2d13ff775c9ebaed1a315a
Sami Badawi <sami@citymaps.com>
2017-02-09 22:04:06 -0500

    Cleaned up unit test for EdgeTracer

--- src/test/scala/org/shapelogic/sc/imageprocessing/EdgeTracerSpec.scala
+++ src/test/scala/org/shapelogic/sc/imageprocessing/EdgeTracerSpec.scala
@@ -20,19 +20,19 @@ import scala.util.Success
 
 object EdgeTracerSpec {
 
-  val _dirURL = "./src/test/resources/data/images/particles/";
-  val _fileFormat = ".gif";
+  val _dirURL = "./src/test/resources/data/images/particles/"
+  val _fileFormat = ".gif"
 
   def filePath(fileName: String): String = {
-    _dirURL + "/" + fileName + _fileFormat;
+    _dirURL + "/" + fileName + _fileFormat
   }
 
   def filePath(fileName: String, fileFormat: String): String = {
-    return _dirURL + "/" + fileName + fileFormat;
+    return _dirURL + "/" + fileName + fileFormat
   }
 
   def filePath(dir: String, fileName: String, fileFormat: String): String = {
-    return dir + "/" + fileName + fileFormat;
+    return dir + "/" + fileName + fileFormat
   }
 
   /**
@@ -84,67 +84,64 @@ class EdgeTracerSpec extends FunSuite with BeforeAndAfterEach {
   val iPerimeter: Double = 54.0
 
   test("Redbox") {
-    val filename = "redbox";
+    val filename = "redbox"
     val imageTry: Try[BufferImage[Byte]] = loadImageTry(filePath(filename, ".png"))
     assert(imageTry.isSuccess)
     val image: BufferImage[Byte] = imageTry.get
-    val foregroundColorInt: Int = 0xff0000;
+    val foregroundColorInt: Int = 0xff0000
     val foregroundColor: Array[Byte] = Array(0, 0, -1)
     val backgroundColor: Array[Byte] = Array(-1, -1, -1)
-    val foregroundColorClose: Array[Byte] = Array(0, 0, 254.toByte) // 0xfe0000;
+    val foregroundColorClose: Array[Byte] = Array(0, 0, 254.toByte) // 0xfe0000
     assert(image != null)
     assertResult(10) { image.width }
     assertResult(10) { image.height }
-    //    assert(image.isRgb())
+    assertResult(3) { image.numBands }
     assertResult(backgroundColor.toSeq) { image.getPixel(0, 0).toSeq } //Background unmasked
-    //    assertResult(0xffffff, image.getPixel(0, 0) & 0xffffff); //Background
     assertResult(foregroundColor.toSeq) { image.getPixel(2, 2) } //Foreground
     println(s"start edgeTracer")
     val edgeTracer: IEdgeTracer = EdgeTracer.fromBufferImage(image, foregroundColorClose, 10, true)
     println(s"end edgeTracer")
     val cch: Polygon = edgeTracer.autoOutline(5, 2)
-    //if 5,5 was used as start point a soft point would have been found
     assertResult(boxPerimeter) { cch.getPerimeter() }
     //    assertResult(4) { cch.getAnnotatedShape().getShapesForAnnotation(PointType.HARD_CORNER).size() }
     //    assertResult(4) { cch.getAnnotatedShape().getShapesForAnnotation(LineType.STRAIGHT).size() }
-    //    printAnnotaions(cch.getAnnotatedShape());
+    //    printAnnotaions(cch.getAnnotatedShape())
     println(s"end of unit test")
   }
 
   test("RedboxInverse") {
-    val filename = "redbox";
+    val filename = "redbox"
     val imageTry: Try[BufferImage[Byte]] = loadImageTry(filePath(filename, ".png"))
     assert(imageTry.isSuccess)
     val image: BufferImage[Byte] = imageTry.get
-    val foregroundColorInt: Int = 0xff0000;
+    val foregroundColorInt: Int = 0xff0000
     val foregroundColor: Array[Byte] = Array(0, 0, -1)
     val backgroundColor: Array[Byte] = Array(-1, -1, -1)
-    val foregroundColorClose: Array[Byte] = Array(0, 0, 254.toByte) // 0xfe0000;
+    val foregroundColorClose: Array[Byte] = Array(0, 0, 254.toByte) // 0xfe0000
 
-    assert(image != null);
+    assert(image != null)
     assertResult(10) { image.width }
     assertResult(10) { image.height }
-    //    assert(image.isRgb());
+    assertResult(3) { image.numBands }
     assertResult(backgroundColor.toSeq) { image.getPixel(0, 0).toSeq } //Background unmasked
     assertResult(foregroundColor.toSeq) { image.getPixel(2, 2).toSeq } //Foreground
     val edgeTracer: IEdgeTracer = EdgeTracer.fromBufferImage(image, foregroundColorClose, 10, false)
-    val cch: Polygon = edgeTracer.autoOutline(1, 1);
-    //if 5,5 was used as start point a soft point would have been found
+    val cch: Polygon = edgeTracer.autoOutline(1, 1)
     assertResult(wholeBobPerimiter) { cch.getPerimeter() }
     //    assertResult(4) { cch.getAnnotatedShape().getShapesForAnnotation(PointType.HARD_CORNER).size() }
     //    assertResult(4) { cch.getAnnotatedShape().getShapesForAnnotation(LineType.STRAIGHT).size() }
-    //    printAnnotaions(cch.getAnnotatedShape());
+    //    printAnnotaions(cch.getAnnotatedShape())
   }
 
   test("Blackbox") {
-    val filename = "blackbox";
+    val filename = "blackbox"
     val imageTry: Try[BufferImage[Byte]] = loadImageTry(filePath(filename, ".png"))
     if (imageTry.isFailure) {
       imageTry.failed.get.printStackTrace()
     }
     assert(imageTry.isSuccess)
     val image: BufferImage[Byte] = imageTry.get
-    assert(image != null);
+    assert(image != null)
     assertResult(10) { image.width }
     assertResult(10) { image.height }
     assertResult(1) { image.numBands }
@@ -159,17 +156,17 @@ class EdgeTracerSpec extends FunSuite with BeforeAndAfterEach {
   }
 
   test("I") {
-    val filename = "I";
+    val filename = "I"
     val imageTry: Try[BufferImage[Byte]] = loadImageTry(filePath("./src/test/resources/data/images/smallThinLetters", filename, ".png"))
     if (imageTry.isFailure) {
       imageTry.failed.get.printStackTrace()
     }
     val image: BufferImage[Byte] = imageTry.get
-    //      int foregroundColor = 255;
     assert(image != null)
     assertResult(30) { image.width }
     assertResult(30) { image.height }
-    assertResult(3) { image.numBands } //XXX this version of png is a RGB
+    //XXX this version of png is a RGB some are index byte
+    assertResult(3) { image.numBands }
     val foregroundColor: Array[Byte] = Array(0, 0, 0)
     val backgroundColor: Array[Byte] = Array(-1, -1, -1)
     assertResult(backgroundColor.toSeq) { image.getPixel(0, 0) } //Background
@@ -180,10 +177,10 @@ class EdgeTracerSpec extends FunSuite with BeforeAndAfterEach {
   }
 
   def printAnnotaions(annotatedShape: AnnotatedShape): Unit = {
-    println("Print annotations:");
-    val map = annotatedShape.getMap(); //Map<Object, Set<GeometricShape2D>>
+    println("Print annotations:")
+    val map = annotatedShape.getMap() //Map<Object, Set<GeometricShape2D>>
     map.foreach { (entry) =>
-      println(entry._1 + ":\n" + entry._2);
+      println(entry._1 + ":\n" + entry._2)
     }
   }
 
@@ -191,13 +188,13 @@ class EdgeTracerSpec extends FunSuite with BeforeAndAfterEach {
     assertResult(-4) { -4 % 8 }
   }
 
-  //  def assertClose(expected: Double, actual: Double): Unit = {
-  //    val absolute: Double = 1;
-  //    val difference: Double = Math.abs(expected - actual);
-  //    val far: Boolean = absolute < difference;
-  //    if (far) {
-  //      println("Expected: " + expected + " not close to actual: " + actual)
-  //      assert(false);
-  //    }
-  //  }
+  def assertClose(expected: Double, actual: Double): Unit = {
+    val absolute: Double = 1
+    val difference: Double = Math.abs(expected - actual)
+    val far: Boolean = absolute < difference
+    if (far) {
+      println("Expected: " + expected + " not close to actual: " + actual)
+      assert(false)
+    }
+  }
 }
