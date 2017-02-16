7687d0a54bb2624efb0d48e4504d957a73149eeb
Sami Badawi <sami@citymaps.com>
2017-02-15 23:26:27 -0500

    Update version number in dialog to 0.8.0

--- src/main/scala/org/shapelogic/sc/util/Constants.scala
+++ src/main/scala/org/shapelogic/sc/util/Constants.scala
@@ -8,7 +8,7 @@ package org.shapelogic.sc.util
  *
  */
 object Constants {
-  val releaseVersion = "0.7.3"
+  val releaseVersion = "0.8.0"
 
   val PRECISION: Double = 0.001
   val BYTE_MASK: Int = 255
