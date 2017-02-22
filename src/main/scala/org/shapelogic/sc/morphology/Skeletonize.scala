63bf9ea1a8e47b29e5cfe4f8dd79bdee7104fc2d
Sami Badawi <sami@citymaps.com>
2017-02-21 20:38:38 -0500

    Start Skeletonize class

---
+++ src/main/scala/org/shapelogic/sc/morphology/Skeletonize.scala
@@ -0,0 +1,12 @@
+package org.shapelogic.sc.morphology
+
+/**
+ * Take specialized binary gray scale byte image
+ *
+ * Based on Hilditch's Algorithm
+ *
+ * http://cgm.cs.mcgill.ca/~godfried/teaching/projects97/azar/skeleton.html
+ */
+class Skeletonize {
+
+}
