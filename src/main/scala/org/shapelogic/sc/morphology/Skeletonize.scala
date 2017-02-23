5389fcc735a22eafd4338cd0134d03cf8da5b596
Sami Badawi <sami@citymaps.com>
2017-02-22 19:52:06 -0500

    Changed Skeletonize into a real operation

--- src/main/scala/org/shapelogic/sc/morphology/Skeletonize.scala
+++ src/main/scala/org/shapelogic/sc/morphology/Skeletonize.scala
@@ -3,30 +3,31 @@ package org.shapelogic.sc.morphology
 import spire.implicits._
 import spire.math._
 import org.shapelogic.sc.image.BufferImage
+import org.shapelogic.sc.image.HasBufferImage
 
 /**
  * Take specialized binary gray scale byte image
  *
- * Based on Hilditch's Algorithm
+ * Code is a port from ImageJ from
+ * https://raw.githubusercontent.com/imagej/imagej1/master/ij/process/BinaryProcessor.java
+ *
+ * Found this reference explaining Hilditch's Algorithm
  *
  * http://cgm.cs.mcgill.ca/~godfried/teaching/projects97/azar/skeleton.html
  *
- * New code is port from ImageJ from
- * https://raw.githubusercontent.com/imagej/imagej1/master/ij/process/BinaryProcessor.java
- * 
- * This does not modify the input image
- * 
+ * Skeletonize does not modify the input image
+ *
  * @param: image input image need to be gray scale
- * @param: inverted if black or
+ * @param: inverted true if black should be background
  */
 class Skeletonize(
     image: BufferImage[Byte],
-    inverted: Boolean) {
+    inverted: Boolean) extends HasBufferImage[Byte] {
 
-  lazy val xMin: Int = image.xMin
-  lazy val xMax: Int = image.xMax
-  lazy val yMin: Int = image.yMin
-  lazy val yMax: Int = image.yMax
+  lazy val xMin: Int = image.xMin + 1
+  lazy val xMax: Int = image.xMax - 1
+  lazy val yMin: Int = image.yMin + 1
+  lazy val yMax: Int = image.yMax - 1
   lazy val width: Int = image.width
   lazy val height: Int = image.height
 
@@ -34,10 +35,10 @@ class Skeletonize(
   val outputImage: BufferImage[Byte] = image.empty
   val outputPixels: Array[Byte] = outputImage.data
 
-  val OUTLINE: Int = 0;
+  val OUTLINE: Int = 0
 
   /**
-   * pixelType: int BYTE=0, SHORT=1, FLOAT=2, RGB=3;
+   * pixelType: int BYTE=0, SHORT=1, FLOAT=2, RGB=3
    */
   def process(pixelType: Int, count: Int): Unit = {
     var p1: Byte = 0
@@ -51,7 +52,7 @@ class Skeletonize(
     var p9: Byte = 0
     var bgColor: Byte = -1 //255
     if (inverted) //XXX inverted
-      bgColor = 0;
+      bgColor = 0
 
     //    val inputPixels = image.data.clone
     var offset: Int = 0
@@ -59,7 +60,7 @@ class Skeletonize(
     var sum: Int = 0
     var rowOffset: Int = image.stride
     cfor(yMin)(_ <= yMax, _ + 1) { y =>
-      offset = xMin + y * width;
+      offset = xMin + y * width
       p2 = inputPixels(offset - rowOffset - 1)
       p3 = inputPixels(offset - rowOffset)
       p5 = inputPixels(offset - 1)
@@ -68,16 +69,19 @@ class Skeletonize(
       p9 = inputPixels(offset + rowOffset)
 
       cfor(image.xMin)(_ <= xMax, _ + 1) { x =>
-        p1 = p2; p2 = p3;
+        p1 = p2
+        p2 = p3
         p3 = inputPixels(offset - rowOffset + 1)
-        p4 = p5; p5 = p6;
+        p4 = p5
+        p5 = p6
         p6 = inputPixels(offset + 1)
-        p7 = p8; p8 = p9;
+        p7 = p8
+        p8 = p9
         p9 = inputPixels(offset + rowOffset + 1)
 
         pixelType match {
           case OUTLINE => {
-            v = p5;
+            v = p5
             if (v != bgColor) {
               if (!(p1 == bgColor || p2 == bgColor || p3 == bgColor || p4 == bgColor
                 || p6 == bgColor || p7 == bgColor || p8 == bgColor || p9 == bgColor))
@@ -128,24 +132,32 @@ class Skeletonize(
   def skeletonize(): Unit = {
     var pass: Int = 0
     var pixelsRemoved: Int = 0
-    //    resetRoi();
-    //    setColor(Color.white);
+    do {
+      pixelsRemoved = thin(pass, table)
+      pass += 1
+      pixelsRemoved += thin(pass, table)
+      pass += 1
+    } while (pixelsRemoved > 0)
+    do { // use a second table to remove "stuck" outputPixels
+      pixelsRemoved = thin(pass, table2)
+      pass += 1
+      pixelsRemoved += thin(pass, table2)
+      pass += 1
+    } while (pixelsRemoved > 0)
+  }
+
+  /**
+   * The active area need to be 1 smaller than the whole image
+   * This can fill the missing.
+   * First line drawing has to be implemented
+   */
+  def drawEdge(): Unit = {
+    //    resetRoi()
+    //    setColor(Color.white)
     //    moveTo(0, 0); lineTo(0, height - 1);
     //    moveTo(0, 0); lineTo(width - 1, 0);
     //    moveTo(width - 1, 0); lineTo(width - 1, height - 1);
     //    moveTo(0, height - 1); lineTo(width /*-1*/ , height - 1);
-    do {
-      pixelsRemoved = thin(pass, table);
-      pass += 1
-      pixelsRemoved += thin(pass, table);
-      pass += 1
-    } while (pixelsRemoved > 0);
-    do { // use a second table to remove "stuck" outputPixels
-      pixelsRemoved = thin(pass, table2);
-      pass += 1
-      pixelsRemoved += thin(pass, table2);
-      pass += 1
-    } while (pixelsRemoved > 0);
   }
 
   def thin(pass: Int, table: Array[Int]): Int = {
@@ -171,32 +183,32 @@ class Skeletonize(
     var pixelsRemoved: Int = 0
     var count: Int = 100
     cfor(yMin)(_ <= yMax, _ + 1) { y =>
-      offset = xMin + y * width;
+      offset = xMin + y * width
       cfor(xMin)(_ <= xMax, _ + 1) { x =>
         p5 = inputPixels(offset)
-        v = p5;
+        v = p5
         if (v != bgColor) {
-          p1 = inputPixels(offset - rowOffset - 1);
-          p2 = inputPixels(offset - rowOffset);
-          p3 = inputPixels(offset - rowOffset + 1);
-          p4 = inputPixels(offset - 1);
-          p6 = inputPixels(offset + 1);
-          p7 = inputPixels(offset + rowOffset - 1);
-          p8 = inputPixels(offset + rowOffset);
-          p9 = inputPixels(offset + rowOffset + 1);
-          index = 0;
-          if (p1 != bgColor) index |= 1;
-          if (p2 != bgColor) index |= 2;
-          if (p3 != bgColor) index |= 4;
-          if (p6 != bgColor) index |= 8;
-          if (p9 != bgColor) index |= 16;
-          if (p8 != bgColor) index |= 32;
-          if (p7 != bgColor) index |= 64;
-          if (p4 != bgColor) index |= 128;
-          code = table(index);
+          p1 = inputPixels(offset - rowOffset - 1)
+          p2 = inputPixels(offset - rowOffset)
+          p3 = inputPixels(offset - rowOffset + 1)
+          p4 = inputPixels(offset - 1)
+          p6 = inputPixels(offset + 1)
+          p7 = inputPixels(offset + rowOffset - 1)
+          p8 = inputPixels(offset + rowOffset)
+          p9 = inputPixels(offset + rowOffset + 1)
+          index = 0
+          if (p1 != bgColor) index |= 1
+          if (p2 != bgColor) index |= 2
+          if (p3 != bgColor) index |= 4
+          if (p6 != bgColor) index |= 8
+          if (p9 != bgColor) index |= 16
+          if (p8 != bgColor) index |= 32
+          if (p7 != bgColor) index |= 64
+          if (p4 != bgColor) index |= 128
+          code = table(index)
           if ((pass & 1) == 1) { //odd pass
             if (code == 2 || code == 3) {
-              v = bgColor;
+              v = bgColor
               pixelsRemoved += 1
             }
           } else { //even pass
@@ -214,6 +226,13 @@ class Skeletonize(
   }
 
   def outline(): Unit = {
-    process(OUTLINE, 0);
+    process(OUTLINE, 0)
   }
+
+  def calc(): BufferImage[Byte] = {
+    process(OUTLINE, 0)
+    outputImage
+  }
+
+  lazy val result = calc()
 }
