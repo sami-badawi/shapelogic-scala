ae9e4d1016107bbefb9ad1d10c7b48107cdd375e
Sami Badawi <sami@citymaps.com>
2017-02-26 19:52:00 -0500

    Made  PixelFollow a sub class of PixelFollowSimilarity

--- src/main/scala/org/shapelogic/sc/imageprocessing/PixelFollow.scala
+++ src/main/scala/org/shapelogic/sc/imageprocessing/PixelFollow.scala
@@ -32,8 +32,8 @@ import org.shapelogic.sc.numeric.NumberPromotion
 abstract class PixelFollow[T: ClassTag, C: ClassTag: Numeric: Ordering](
     image: BufferImage[T],
     maxDistance: C,
-    similarIsMatch: Boolean)(implicit promoterIn: NumberPromotion.Aux[T, C]) {
-  val verboseLogging = false
+    similarIsMatch: Boolean)(implicit promoterIn: NumberPromotion.Aux[T, C])
+    extends PixelFollowSimilarity[T](image, similarIsMatch) {
 
   // =============== lazy init ===============
 
@@ -44,60 +44,7 @@ abstract class PixelFollow[T: ClassTag, C: ClassTag: Numeric: Ordering](
     implicitly[Ordering[C]],
     promoterIn)
 
-  /**
-   * true means that a pixel is handled
-   */
-  lazy val handledPixelImage = new BufferBooleanImage(image.width, image.height, 1)
-
-  // lazy get normal image properties for faster access
-  lazy val width: Int = image.width
-  lazy val height: Int = image.height
-  lazy val numBands = image.numBands
-  lazy val stride = image.stride
-  lazy val xMin: Int = image.xMin
-  lazy val xMax: Int = image.xMax
-  lazy val yMin: Int = image.yMin
-  lazy val yMax: Int = image.yMax
-  lazy val cyclePoints = image.cyclePoints
-
-  var _dirs = new Array[Boolean](Constants.DIRECTIONS_AROUND_POINT)
-  val STEP_SIZE_FOR_4_DIRECTIONS = 2
-
-  lazy val maxLength = scala.math.min(10000, image.pixelCount + 4)
-
-  // =============== abstract ===============
-
-  def inputImage: BufferImage[T]
-  def outputImage: BufferImage[T]
-
   // =============== util for abstract ===============
-
-  def pixelIsHandledIndex(index: Int): Boolean = {
-    handledPixelImage.getChannel(x = index, y = 0, ch = 0) //XXX better way
-  }
-
-  def markPixelHandled(x: Int, y: Int): Unit = {
-    handledPixelImage.setChannel(x, y, 0, true)
-  }
-
-  def pixelIsHandled(x: Int, y: Int): Boolean = {
-    handledPixelImage.getChannel(x, y, ch = 0)
-  }
-
-  def newSimilarIndex(index: Int): Boolean = {
-    pixelDistance.similarIndex(index) && !pixelIsHandledIndex(index)
-  }
-
-  def newSimilar(x: Int, y: Int): Boolean = {
-    newSimilarIndex(image.getIndex(x, y))
-  }
-
-  /**
-   *  Use XOR to either handle colors close to reference color or far away.
-   */
-  def matchInBounds(x: Int, y: Int): Boolean = {
-    return pixelDistance.matchInBounds(x, y)
-  }
 
   /**
    * Set reference color to the color of a point
@@ -113,34 +60,7 @@ abstract class PixelFollow[T: ClassTag, C: ClassTag: Numeric: Ordering](
     pixelDistance.setReferencePointArray(iArray)
   }
 
-  def copyPixel(x: Int, y: Int): Unit = {
-    cfor(0)(_ < numBands, _ + 1) { ch =>
-      outputImage.setChannel(x, y, ch, inputImage.getChannel(x, y, ch))
-    }
-  }
-
   // =============== util ===============
 
-  def findTop(startX: Int, startY: Int): Option[(Int, Int)] = {
-    var x = startX
-    var y = startY
-    if (!matchInBounds(x, y)) {
-      println(s"First point matchInBounds($x, $y) not matchInBounds. Exit")
-      return None
-    }
-    //Find top point matchInBounds
-    do {
-      y -= 1
-    } while (matchInBounds(x, y))
-    y += 1
-    //Find leftmost top point matchInBounds
-    do {
-      x -= 1
-    } while (matchInBounds(x, y))
-    x += 1
-    if (verboseLogging)
-      println(s"Top point matchInBounds($x, $y) found start traceEdge(x, y, 2)")
-    Try((x, y)).toOption
-  }
 
 }
